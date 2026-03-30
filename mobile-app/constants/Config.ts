4703beeb4a9a02621caaf444d29b4afc3bb2fe9e
ZhenZhen09 <palomadodlsp@gmail.com>
2026-03-30 21:49:14 +0800

    config

--- mobile-app/constants/Config.ts
+++ mobile-app/constants/Config.ts
@@ -8,7 +8,7 @@ export const Config = {
 // 1. Change YOUR_LOCAL_IP to your actual machine IP (e.g. 192.168.1.13)
 // 2. Change YOUR_RENDER_URL to your actual render .onrender.com URL
    API_URL: IS_PRODUCTION
-     ? 'https://rider-web-api-2.onrender.com'
+     ? 'https://rrs-vhgr.onrender.com'
      : 'http://10.182.225.154:3001', // Note: Both use port 3001 now   
    GEOAPIFY_KEY: 'e981beca841349698124675a91674f3a',
 };
