633cf48786ad30e778802083a0b54ea51fb1e0ae
ZhenZhen09 <palomadodlsp@gmail.com>
2026-04-12 12:34:52 +0800

    map modal 2

--- src/app/pages/Admin/DispatchConsole.tsx
+++ src/app/pages/Admin/DispatchConsole.tsx
@@ -37,6 +37,7 @@ import {
   DialogDescription,
   DialogFooter
 } from "../../components/ui/dialog";
+import { Button } from "../../components/ui/button";
 import { Textarea } from "../../components/ui/textarea";
 import { Label } from "../../components/ui/label";
 import { Checkbox } from "../../components/ui/checkbox";
